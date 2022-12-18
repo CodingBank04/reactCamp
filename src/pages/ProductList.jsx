2b895be5ce4a224c4dd0c3844ffc51d89c20f968
Guru Tech <guruautomation04@gmail.com>
2022-12-18 13:54:23 -0700

    dec 12 22 grid

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
 export default function ProductList() {
+
+    const [products, setProducts] = useState([])
+
     return (
         <div>
             <Table celled>
