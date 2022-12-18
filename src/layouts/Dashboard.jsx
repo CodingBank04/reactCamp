015a00bb5ac2cbc976a900a28c97beaf114a08b7
Guru Tech <guruautomation04@gmail.com>
2022-12-18 12:23:12 -0700

    commit master

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProductList from '../pages/ProductList'
+import Category from './Category'
+import Navi from './Navi'
+
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Navi />
+            <Category />
+            <ProductList />
+        </div>
+
+
+    )
+}
