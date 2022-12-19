58085b47aa2701624a1170894af4a00569224a17
Guru Tech <guruautomation04@gmail.com>
2022-12-19 13:23:09 -0700

    j

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import React, { useState, useEffect } from 'react'
+import { Icon, Menu, Table } from 'semantic-ui-react'
+import ProductService from '../services/productService';
 
 export default function ProductList() {
 
-    const [products, setProducts] = useState([])
+    //const [products, setProducts]=useState([])
+    //lifecycle hook
+    const [products, setProuducts] = useState([]);
+
+    useEffect(() => {
+        let proudctService = new ProudctService()
+        proudctService.getProducts().then(results => setProuducts(results.data.data))
+
+    })
 
     return (
         <div>
@@ -11,31 +20,26 @@ export default function ProductList() {
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell>Header</Table.HeaderCell>
-                        <Table.HeaderCell>Header</Table.HeaderCell>
-                        <Table.HeaderCell>Header</Table.HeaderCell>
+                        <Table.HeaderCell>Birim fiyati</Table.HeaderCell>
+                        <Table.HeaderCell>Stok Adedi</Table.HeaderCell>
+                        <Table.HeaderCell>Aciklama</Table.HeaderCell>
+                        <Table.HeaderCell>Kategori</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
-                    <Table.Row>
-                        <Table.Cell>
-                            <Label ribbon>First</Label>
-                        </Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                    </Table.Row>
-                </Table.Body>
+                    {products.map(product => (
+                        <Table.Row>
+                            <Table.Cell>Cell</Table.Cell>
+                            <Table.Cell>Cell</Table.Cell>
+                            <Table.Cell>Cell</Table.Cell>
+                            <Table.Cell>Cell</Table.Cell>
+                            <Table.Cell>Cell</Table.Cell>
+                        </Table.Row>
 
+                    ))
+                    }
+                </Table.Body>
                 <Table.Footer>
                     <Table.Row>
                         <Table.HeaderCell colSpan='3'>
